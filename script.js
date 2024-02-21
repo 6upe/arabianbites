db7947b3de842cbbc10c17a0c501b942865c7841
Bupe Katongo <107362236+6upe@users.noreply.github.com>
2024-02-21 10:40:26 -0800

    styled 8

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let cart = []; // Array to store cart items
 let bill = 0;
 // Function to update cart badge count
 function updateCartBadge() {
-  const badge = document.getElementById('cartBadge');
+  const badge = document.getElementById("cartBadge");
   badge.innerText = cart.length;
 }
 
 // Function to add item to cart
 function addToCart(item) {
-    console.log('added to cart: ', item)
+  console.log("added to cart: ", item);
   cart.push(item);
   updateCartBadge();
 }
@@ -23,98 +23,97 @@ function removeFromCart(index) {
 
 // Function to display cart items in modal
 function showCartItems() {
-    const cartItemsDiv = document.getElementById('cartItems');
-    cartItemsDiv.innerHTML = '';
+  const cartItemsDiv = document.getElementById("cartItems");
+  cartItemsDiv.innerHTML = "";
 
-    // Variable to store the total price
-    let totalPrice = 0;
-  
-    cart.forEach((item, index) => {
-      const itemDiv = document.createElement('div');
-      itemDiv.classList.add('cart-item');
-      itemDiv.innerHTML = `
-        <div class="fh5co-food-desc" style="display: flex; align-items: center; justify-content: space-between">
+  // Variable to store the total price
+  let totalPrice = 0;
+
+  cart.forEach((item, index) => {
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("cart-item");
+    itemDiv.innerHTML = `
+        <div class="fh5co-food-desc" style="display: flex; align-items: center; justify-content: space-between; margin-top: 5px;">
           <figure style="margin-right: 10px;">
-            <img width="20px" src="${item.img}" class="img-responsive" alt="img">
+            <img width="100px" src="${item.img}" class="img-responsive" alt="img">
           </figure>
-          <div>
+
+          <div style="display: flex; align-items: center; justify-content: space-between; width: 50%;">
             <p style="margin: 0;">${item.name}</p>
-            <p style="margin: 0;">K${item.price}</p>
+            <p style="margin: 0; font-weight: bold; color: green;">K${item.price}</p>
           </div>
-          <div class="fh5co-food-pricing">
-          <button class="btn btn-danger" style="margin-top: 10px;" onclick="removeFromCart(${index})">Remove</button>
-        </div>
+          <button class="btn btn-danger" onclick="removeFromCart(${index})">
+          
+          <i class="fa fa-trash"  aria-hidden="true"></i>
+          </button>
         </div>
         
       `;
-      cartItemsDiv.appendChild(itemDiv);
-      // Add item price to the total
-      totalPrice += item.price;
-    });
+    cartItemsDiv.appendChild(itemDiv);
+    // Add item price to the total
+    totalPrice += item.price;
+  });
 
-    // Display total next to checkout button
-    const totalDiv = document.getElementById('totalDiv');
-    totalDiv.innerHTML = `Total: K${totalPrice}`;
-    bill = totalPrice;
-
-  }
-
+  // Display total next to checkout button
+  const totalDiv = document.getElementById("totalDiv");
+  totalDiv.innerHTML = `Total: K${totalPrice}`;
+  bill = totalPrice;
+}
 
 // Initialize cart button
-document.getElementById('cartBtn').addEventListener('click', function() {
+document.getElementById("cartBtn").addEventListener("click", function () {
   showCartItems();
-  $('#cartModal').modal('show');
+  $("#cartModal").modal("show");
 });
 
 // Function to send order message to WhatsApp
 function sendOrder(orderDetails) {
-    var whatsappNumber = "+260962893773";
-    var message = "Hello! Here is the order summary:\n\n" + orderDetails;
-    window.open(
-      "https://api.whatsapp.com/send?phone=" +
-        whatsappNumber +
-        "&text=" +
-        encodeURIComponent(message)
-    );
-  }
-  
-  // Initialize checkout button
-  document.getElementById('checkoutBtn').addEventListener('click', function() {
-    // Gather information about the order
-    let orderDetails = 'Order Summary:\n';
-    cart.forEach((item, index) => {
-      orderDetails += `${index + 1}. ${item.name} - K${item.price}\n`;
-    });
-    orderDetails += `\nTotal : ${bill}\n`;
-  
-    // Send WhatsApp message with order details
-    sendOrder(orderDetails);
-  
-    // Clear the cart after checkout
-    cart = [];
-    updateCartBadge();
-  
-    // Close the modal
-    $('#cartModal').modal('hide');
-  
-    // Show success message
-    alert('Order placed successfully!');
-  });
+  var whatsappNumber = "+260962893773";
+  var message = "Hello! Here is the order summary:\n\n" + orderDetails;
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      whatsappNumber +
+      "&text=" +
+      encodeURIComponent(message)
+  );
+}
 
-  // Sample menu items
+// Initialize checkout button
+document.getElementById("checkoutBtn").addEventListener("click", function () {
+  // Gather information about the order
+  let orderDetails = "Order Summary:\n";
+  cart.forEach((item, index) => {
+    orderDetails += `${index + 1}. ${item.name} - K${item.price}\n`;
+  });
+  orderDetails += `\nTotal : ${bill}\n`;
+
+  // Send WhatsApp message with order details
+  sendOrder(orderDetails);
+
+  // Clear the cart after checkout
+  cart = [];
+  updateCartBadge();
+
+  // Close the modal
+  $("#cartModal").modal("hide");
+
+  // Show success message
+  alert("Order placed successfully!");
+});
+
+// Sample menu items
 const menuItems = [
-    { img: './Buffalo Wings.jpg', name: 'Buffalo Wings', price: 120 },
-    { img: './Fatteh.jpg', name: 'Fatteh', price: 150 },
-    { img: './chips.jpg', name: 'Chips Big Plate', price: 60 },
-    { img: './Hummus Moutabal.jpg', name: 'Hummus Moutabal', price: 130 }
-  ];
-  
+  { img: "./Buffalo Wings.jpg", name: "Buffalo Wings", price: 120 },
+  { img: "./Fatteh.jpg", name: "Fatteh", price: 150 },
+  { img: "./chips.jpg", name: "Chips Big Plate", price: 60 },
+  { img: "./Hummus Moutabal.jpg", name: "Hummus Moutabal", price: 130 },
+];
 
 // Initialize menu
-const menuList = document.getElementById('menu');
-menuItems.forEach(item => {
-  const li = document.createElement('li');
-  li.style.padding = '10px';
+const menuList = document.getElementById("menu");
+menuItems.forEach((item) => {
+  const li = document.createElement("li");
+  li.style.padding = "10px";
   li.innerHTML = `
     <div class="fh5co-food-desc">
       <figure>
@@ -132,11 +131,10 @@ menuItems.forEach(item => {
     </div>
   `;
   menuList.appendChild(li);
-  
+
   // Add event listener to the newly created button
-  const addToCartBtn = li.querySelector('.addToCartBtn');
-  addToCartBtn.addEventListener('click', function() {
+  const addToCartBtn = li.querySelector(".addToCartBtn");
+  addToCartBtn.addEventListener("click", function () {
     addToCart(item);
   });
 });
-
